afffa42bb392a705296c5369fecb930e476cb221
colbyallenc <colbyallenc@gmail.com>
2017-02-22 13:51:59 -0500

    iteration 1,2,3 calculating price of all quantities

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,60 @@
-function deleteItem(e){
+
+
+
+var item = document.getElementsByClassName('item');
+console.log(item);
+
+  function getTotalPrice() {
+    var subArray= [];
+    for (var i = 0; i<item.length; i++){
+      var price = parseInt(document.getElementsByClassName('price')[i].innerHTML);
+      console.log(price);
+      var quantity= document.getElementsByClassName('quantity')[i].value;
+      console.log(quantity);
+      var itemTotal = ('$' + price * quantity);
+      console.log (itemTotal);
+      document.getElementsByClassName('subtotal')[i].innerHTML=itemTotal;
+      subArray.push(price * quantity);
+}
+console.log(subArray);
+// loop through subArray and add everything up
+var grandTotal = subArray.reduce(function(a,b){
+  return a + b;
+}, 0);
+console.log(grandTotal);
+
+document.getElementById('totalPrice').innerHTML=grandTotal;
 
 }
 
-function getPriceByProduct(itemNode){
 
-}
 
-function updatePriceByProduct(productPrice, index){
 
-}
 
-function getTotalPrice() {
 
-}
+// var subArray= [];
+//
+// function updateTotalPrice() {
+//   var blah = document.getElementsByClassName('price')
+//
+//   for (var i = 0; i<blah.length; i++){
+//     var sumTotal= document.getElementsByClassName('subtotal')[i];
+//   subArray.push(sumTotal);
+// }
+// }
 
-function createQuantityInput(){
+ // console.log(subArray);
 
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
-};
+//
+// window.onload = function(){
+//   var calculatePriceButton = document.getElementById('calc-prices-button');
+//   // var createItemButton = document.getElementById('new-item-create');
+//   // var deleteButtons = document.getElementsByClassName('btn-delete');
+//
+//   calculatePriceButton.onclick = getTotalPrice;
+//   // createItemButton.onclick = createNewItem;
+//
+//   for(var i = 0; i<deleteButtons.length ; i++){
+//     deleteButtons[i].onclick = deleteItem;
+//   }
+// };
